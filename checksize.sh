4fefbaa6fa279a4cd052763239315d00c704a119
Long Zer <gialongminecraft@gmail.com>
2023-04-02 08:53:10 +0700

    Revert "initial"
    This reverts commit f58ee888c34d713c660bda61073c278e11263f0b.

--- checksize.sh
+++
@@ -1,1 +0,0 @@
-du -sh   ./minecraftServer/world ./minecraftServer/world_nether ./minecraftServer/world_the_end ./minecraftServer/logs
--- start_server.sh
+++
@@ -1,5 +0,0 @@
-tmux new-session -d -s minecraft
-tmux new-session -d -s playit
-tmux send-keys -t playit 'playit' Enter
-tmux send-keys -t minecraft 'cd minecraftServer' Enter
-tmux send-keys -t minecraft 'java -jar -Xms1024M -Xmx7168M server.jar' Enter
